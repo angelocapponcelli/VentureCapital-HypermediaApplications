a9699fa56b53dd6834662bff5c4fc4ce210c3c78
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-05-10 16:01:26 +0200

    Test tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,19 @@
     Default layout used by all the page
 -->
 <template>
-    <div class = 'page'>
-        <TheHeader />
+    <div class='page'>
+        <Navbar />
         <slot />
         <TheFooter />
     </div>
 </template>
 
 <style>
-    .page {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        background-color: lightgoldenrodyellow;
-        margin: 0;
-    }
+.page {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: lightgoldenrodyellow;
+    margin: 0;
+}
 </style>
