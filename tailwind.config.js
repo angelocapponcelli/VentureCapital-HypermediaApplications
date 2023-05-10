5e26c51166e7dd99466c638abd2a49b0503ad851
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-05-10 20:39:42 +0200

    Header update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,24 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
+    colors: {
+      // custom color palette
+      white: "#ffffff",
+      "primary-color": "#5B45F8",
+      "headings-black": "#494850",
+      gray: "#9795B5",
+      color: {
+        200: "#FAFAFF",
+        300: "#F9F9FF",
+        400: "#F2F1FA",
+        500: "#E7E6F2",
+        600: "#D4D2E3",
+        700: "#BCBACD",
+        800: "#ADABC3",
+        900: "#8D8BA7",
+        1000: "#767494",
+      },
+    },
     fontFamily: {
       sans: ['"DM Sans"', "sans-serif"],
     },
