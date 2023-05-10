c9dc397ade0667299948fe80725f72bca236f7f2
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-05-10 19:05:19 +0200

    Header improvement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        '~/assets/css/general.css'
-    ],
-    modules: [
-        '@nuxtjs/supabase'
-    ],
-    ssr: true
-})
+  css: ["~/assets/css/general.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@nuxtjs/supabase"],
+  ssr: true,
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" }, //import from google font
+        { rel: "preconnect", href: "https://fonts.gstatic.com" }, //import from google font
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap", //import from google font
+        },
+      ],
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue,ts,jsx,tsx}"],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
+    fontFamily: {
+      sans: ['"DM Sans"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
