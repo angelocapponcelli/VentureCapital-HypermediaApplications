0718fc87906f25639ae4ab41dace18cb72c0b897
William Zeni <williamzeni56@gmail.com>
2023-05-21 17:31:08 +0200

    about us page quasi completa

---
+++ components/AboutUs/structureLinks.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="structbar" >
+        
+        <div class="link" v-for="link in links" >
+            <a @click="scroll(link.link)">{{ link.title }}</a>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['links'],
+    //paragraphLinks has to be an object like {link: xxx, title: xxx }
+    methods:{
+        scroll(refName){
+            console.log(refName)
+            //document.getElementById(link).scrollIntoView({ behavior: 'smooth' });
+            var element = document.getElementById(refName)
+            element.scrollIntoView({block: "start", inline: "start", behavior:"smooth"})
+            
+        }
+    }
+}
+</script>
+
+<style>
+.structbar{
+    background-color: #F2F1FA;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    padding-top: 2vh;
+    padding-bottom: 2vh;
+}
+
+.link{
+    margin-left: 5vw;
+    margin-right: 5vw;
+    color:#7d7b97; 
+    font-weight: bold;
+    font-size:1.2em;
+}
+
+.link:hover{
+    border-bottom: solid 3px #a9a4cd;
+}
+
+a:hover{
+    color: #a9a4cd;
+
+}
+
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -2,22 +2,145 @@
     About us page
 -->
 <template>
-    <!--
-                Instead of having the two structures written directly on the page, it was preferred to use components as they had the same layout.
-                This also allows to have a much cleaner and easy to read 'code'
-            -->
-    <main>
-        <TextImage description="This is a description for the Dog's Shelter. The best place where dogs can rest. " />
-        <TextImage description="This is the second description for the Dog's Shelter. The best place where dogs can rest."
-            :isReverse=true />
-    </main>
+    <Breadcrumb :crumbs="[
+                { label: 'About Us', link: '/abouts' }]" />
+    <AboutUsStructureLinks :links = "[
+        {
+            link:'whoweare', //should be the same of the id section
+            title: 'Who we are'
+        }, 
+        {
+            link: 'companymission',
+            title: 'Company mission'
+
+        }, 
+        {
+            link: 'partners',
+            title: 'Partners'
+
+        }
+    ]"></AboutUsStructureLinks>
+   
+    ,<!--<div>
+        <table>
+            <tr>
+                <th> Who we are</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum sit amet justo donec enim diam vulputate. Aliquet lectus proin nibh nisl condimentum id. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Diam quam nulla porttitor massa id neque. Massa tincidunt dui ut ornare lectus. Netus et malesuada fames ac turpis egestas integer eget. Amet facilisis magna etiam tempor orci eu lobortis elementum. Velit sed ullamcorper morbi tincidunt ornare massa. Dictum non consectetur a erat nam at. Ac placerat vestibulum lectus mauris ultrices eros in. Faucibus pulvinar elementum integer enim neque volutpat. Neque sodales ut etiam sit amet nisl purus in mollis. Sed adipiscing diam donec adipiscing tristique risus nec. Nisl tincidunt eget nullam non nisi.</td>
+                <td>IMMAGINE</td>
+            </tr>
+        </table>
+    </div>-->
+    <div class="card" id="whoweare">
+        <div class="content-section">
+            <h2>Who we are</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="image-section">
+            <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+        </div>
+  </div>
+  <div class="card" id="companymission">
+    <div class="image-section">
+            <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+        </div>
+        <div class="content-section">
+            <h2>Company mission</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+  </div>
+  <div class="card" id="partners">
+        <div class="content-section">
+            <h2>Partners</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="image-section">
+            <table>
+                <tr>
+                    <td>
+                        <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+                    </td>
+                    <td>
+                        <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+                    </td>
+                    <td>
+                        <img src="_nuxt/assets/img/startup/1.png" alt="Immagine" />
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+  </div>
+    
 </template>
 
+<script>
+</script>
 <style>
-main {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+ 
+    .card {
+        display: flex;
+        border: 1px solid #ccc;
+        background-color:#F2F1FA ;
+        border-radius: 35px;
+        padding-top: 2vh;
+        padding-bottom: 2vh;
+        padding-left: 2vh;
+        padding-right: 2vh;
+        margin-left: 2vw;
+        margin-right: 2vw;
+        margin-top: 3vw;
+        margin-bottom: 3vw;
+    }
+    
+    .image-section {
+        flex: 1;
+    }
+
+    .image-section img{
+        border-radius: 45px;
+        display: block;
+        width: 100%;
+        height: auto;
+        padding-top: 2vh;
+        padding-bottom: 2vh;
+        padding-left: 2vh;
+        padding-right: 2vh;
+    }
+    
+    .content-section {
+        flex: 1;
+        padding-top: 2vh;
+        padding-bottom: 2vh;
+        padding-left: 2vh;
+        padding-right: 2vh;
+        color: #494850;
+        font-size:1.2em;
+      
+    }
+    
+    .content-section h2 {
+        margin-top: 1vh;
+        margin-bottom: 1vh;
+        color:#504e62; 
+        font-weight: bold;
+        font-size:1.5em;
+    }
+
+    @media (max-width: 767px) {
+      .card {
+        flex-direction: column;
+      }
+      
+      .image-section {
+        order: 2;
+      }
+    }
 </style>
