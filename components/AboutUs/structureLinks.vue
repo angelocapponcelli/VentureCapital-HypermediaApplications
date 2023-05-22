45969bdcdd8851bb527aefd5d3d81291f2c2e206
William Zeni <williamzeni56@gmail.com>
2023-05-22 15:09:35 +0200

    prima versione animazione funzionante

--- components/AboutUs/structureLinks.vue
+++ components/AboutUs/structureLinks.vue
@@ -1,53 +1,83 @@
 <template>
-    <div class="structbar" >
-        
-        <div v-for="link in links" >
-            <a class="link" @click="scroll(link.link)">{{ link.title }}</a>
-        </div>
-        
+    <div :class="{'structbar': true, 'fixed': isFixed}" id="fixed">
+      <div v-for="link in links" :key="link.title">
+        <a class="link" @click="scroll(link.link)">{{ link.title }}</a>
+      </div>
     </div>
-</template>
-
-<script>
-export default{
+  </template>
+  
+  <script>
+  export default {
     props: ['links'],
-    //paragraphLinks has to be an object like {link: xxx, title: xxx }
-    methods:{
-        scroll(refName){
-            var element = document.getElementById(refName)
-            element.scrollIntoView({block: "start", inline: "start", behavior:"smooth"})
-            
+    data() {
+      return {
+        isFixed: false,
+        componentTop: 0,
+        scrollPosition: 0
+      };
+    },
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ block: 'start', inline: 'start', behavior: 'smooth' });
+      },
+      handleScroll() {
+        this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+        // Se la posizione corrente della pagina è maggiore della posizione iniziale del componente,
+        // impostiamo isFixed su true per applicare la classe 'fixed'
+        if (this.scrollPosition > this.componentTop) {
+          this.isFixed = true;
+        } else {
+          this.isFixed = false;
         }
+      }
+    },
+    mounted() {
+      this.componentTop = this.$el.offsetTop;
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleScroll);
     }
-}
-</script>
-
-<style>
-.structbar{
-    background-color: #F2F1FA;
+  };
+  </script>
+  
+  <style>
+  .structbar {
+    background-color: #f2f1fa;
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: center;
     padding-top: 2vh;
     padding-bottom: 2vh;
-}
-
-.link{
+    width: 100%;
+  }
+  
+  .fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 999;
+  }
+  
+  .link {
     margin-left: 5vw;
     margin-right: 5vw;
-    color:#7d7b97; 
+    color: #7d7b97;
     font-weight: bold;
-    font-size:1.2em;
-}
-
-.link:hover{
+    font-size: 1.2em;
+  }
+  
+  .link:hover {
     border-bottom: solid 3px #a9a4cd;
-}
-
-a:hover{
+  }
+  
+  a:hover {
     color: #a9a4cd;
-
-}
-
-</style>
+  }
+  </style>
+  
+  
