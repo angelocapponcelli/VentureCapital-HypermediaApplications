22b5ac3a460485fbdd266789a3ad098cbcb0884b
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-05-23 18:26:24 +0200

    Fix animation

--- pages/index.vue
+++ pages/index.vue
@@ -19,9 +19,9 @@
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
                         stroke="currentColor" class="w-5 h-5">
-                                                                                                                                                                                                                                                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                                                                                                                                                                                                                                                                                                </svg>
-                                                                                                                                                                                                                                                                                            </a-->
+                                                                                                                                                                                                                                                                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                                                                                                                                                                                                                                                                                                    </svg>
+                                                                                                                                                                                                                                                                                                </a-->
             </div>
         </div>
 
@@ -278,11 +278,11 @@ function truncate(input) {
     transition-delay: 100ms;
 }
 
-.hiddenItemChild:nth-child(8) {
+.hiddenItemChild:nth-child(3) {
     transition-delay: 200ms;
 }
 
-.hiddenItemChild:nth-child(6) {
+.hiddenItemChild:nth-child(4) {
     transition-delay: 300ms;
 }
 
@@ -290,15 +290,15 @@ function truncate(input) {
     transition-delay: 400ms;
 }
 
-.hiddenItemChild:nth-child(7) {
+.hiddenItemChild:nth-child(6) {
     transition-delay: 500ms;
 }
 
-.hiddenItemChild:nth-child(4) {
+.hiddenItemChild:nth-child(7) {
     transition-delay: 600ms;
 }
 
-.hiddenItemChild:nth-child(3) {
+.hiddenItemChild:nth-child(8) {
     transition-delay: 700ms;
 }
 </style>
