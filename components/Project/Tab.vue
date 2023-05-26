33a83e5cb12483c0b6f992d73c4480ee581a6ff7
Mattia Carini <mattia.carini1998@gmail.com>
2023-05-26 22:38:52 +0200

    Fix project tab img src

--- components/Project/Tab.vue
+++ components/Project/Tab.vue
@@ -4,7 +4,7 @@
 
     <!-- cover image TODO: :src="pathImage" -->
     <img class="object-center object-cover  md:w-40 w-full aspect-square rounded-xl"
-      :data-lazy="config.SUPABASE_ASSETS_URL + image + '.png'" alt="Image">
+      :src="config.SUPABASE_ASSETS_URL + image + '.png'" alt="Image">
 
     <!-- startup overview -->
     <div class="flex-auto content-center flex-col space-y-3 align-middle m-auto md:pt-0 pt-4">
